2dc6bbcfbdcff14c390db71b35a0e88f3fa79927
Fred <fgaurat@gmail.com>
2021-11-09 17:05:47 +0100

    ok

--- todo-react-form/src/app/app.component.ts
+++ todo-react-form/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'todo-react-form';
+
+  todoForm = this.fb.group({
+    title:["",Validators.required],
+    completed:[false],
+    dueDate:[new Date()],
+    aliases: this.fb.array([
+      this.fb.control('')
+    ])
+
+  })
+
+
+  constructor(private fb:FormBuilder){
+  }
+  ngOnInit(): void {
+    this.todoForm.valueChanges.subscribe((data:any) => console.log(data))
+  }
+
+  onSubmit(){}
+
+  get aliases() {
+    return this.todoForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+
 }
--- todo-react-form/src/app/app.module.ts
+++ todo-react-form/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
+    imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
